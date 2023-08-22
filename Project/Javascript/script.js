3e5a728cf8421f2f5df6a866960279285a892a73
ManideepKadari <manideepkadari7680@gmail.com>
2023-08-22 18:21:01 +0530

    Added React and Node js

--- Project/Javascript/script.js
+++ Project/Javascript/script.js
@@ -1,1 +1,6 @@
-alert("Hello");
+// alert("Hello");
+let hello="Hello World!";
+const path=require("path");
+
+console.log(__dirname);
+console.log(`This is you answer:${path.basename(__filename)}`);
